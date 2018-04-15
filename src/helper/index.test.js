d890e71f3034c634b5524b1b412804b9efb5818c
Shirokovskikh Sergey <hellomoto177@yandex.ru>
2018-04-15 15:50:52 +0500

    100 percent coverage

---
+++ src/helper/index.test.js
@@ -0,0 +1,13 @@
+import { maxNumber } from './index'
+
+describe('maxNumber', () => {
+  describe('given an empty array', () => {
+    it('returns 0', () => {
+      expect(maxNumber([])).toEqual(0)
+    })
+
+    it('returns the max number', () => {
+      expect(maxNumber([1, 2, 3])).toEqual(3)
+    })
+  })
+})
